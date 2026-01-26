39a5fe132ea756e9544954adf41ef194d4e7e935
ocean <ocean@ocean.lol>
2026-01-26 11:41:42 +0000

    make unlisted entries not viewable

--- src/routes/entry.ts
+++ src/routes/entry.ts
@@ -1,7 +1,7 @@
 
 import { Router } from 'express';
 import db from '../db';
-import { eq, desc, asc, count, sql } from 'drizzle-orm';
+import { eq, and, sql } from 'drizzle-orm';
 import { entries, entryTags, tags } from '../db/schema';
 import { getFileType } from '../shared';
 import config from '../config';
@@ -9,7 +9,7 @@ import config from '../config';
 const router = Router();
 
 router.get('/:id', async (req, res) => {
-    let entry = await db.select().from(entries).where(eq(entries.filename, req.params.id));
+    let entry = await db.select().from(entries).where(and(eq(entries.filename, req.params.id), eq(entries.listed, true)));
     if (entry.length == 0) return res.sendStatus(404);
     let e = entry[0];
     let theTags = await db.select().from(entryTags).where(eq(entryTags.entry, e.id)).orderBy(entryTags.order).leftJoin(tags, eq(tags.id, entryTags.tag));
