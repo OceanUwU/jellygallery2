7d9c214e615b0b7ec18210b26ab3b4b773fe04e9
ocean <ocean@ocean.lol>
2026-05-02 20:25:43 +0100

    dark mode

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,22 @@
+var theme = localStorage.getItem("theme");
+if (theme == null) theme = "light";
+
+function changeTheme() {
+    document.getElementsByTagName("body")[0].setAttribute("data-bs-theme", theme);
+    document.getElementById('themeIcon').classList.remove('bi-moon-stars-fill');
+    document.getElementById('themeIcon').classList.remove('bi-sun-fill');
+    if (theme == "light")
+        document.getElementById('themeIcon').classList.add('bi-sun-fill');
+    else
+        document.getElementById('themeIcon').classList.add('bi-moon-stars-fill');
+}
+changeTheme();
+document.getElementById('themeToggle').onclick = () => {
+    theme = theme == "light" ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+    changeTheme();
+}
+
 document.getElementById('searchBox').value = null;
 document.getElementById('searchButton').onclick = () => {
     let url = new URL(location.origin);
